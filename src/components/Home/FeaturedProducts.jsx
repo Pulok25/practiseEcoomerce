3f11dd17e96fd3cf476bc410ef096d9a7226bf2c
Md Akib Zaman Pulok <pulokjack@gmail.com>
2026-03-29 21:14:56 +0600

    Update FeaturedProducts component to use dynamic product data for images and text

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -14,12 +14,12 @@ const FeaturedProducts = () => {
             <div key={product.id} className="card bg-base-100 w-96 shadow-sm">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={product.img}
       alt="Shoes" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <h2 className="card-title">{product.title}</h2>
+    <p>{product.description}</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
     </div>
