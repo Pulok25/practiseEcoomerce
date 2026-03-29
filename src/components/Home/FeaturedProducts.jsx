37793eee67bbbbeee7a391a07c66dfbc774f4a97
Md Akib Zaman Pulok <pulokjack@gmail.com>
2026-03-29 21:09:06 +0600

    second commit

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -11,7 +11,20 @@ const FeaturedProducts = () => {
       {
         products.map(product => {
           return (
-            
+            <div key={product.id} className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
           )
 
         })
