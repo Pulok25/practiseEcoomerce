d88d318d142db3d403b537cd38e0b9eecc9c40e7
Md Akib Zaman Pulok <pulokjack@gmail.com>
2026-03-30 17:18:13 +0600

    some code added

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const LoginForm = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.name.value;
+    const password = e.target.password.value;
+    console.log(email,password);
+    
+
+  };
   return (
     <div>
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+        <div className="card-body">
+          <form onSubmit={(e) => {
+            handleLogin(e);
+          }} >
+            <input 
+            type="email" 
+            placeholder='Enter Your Email' 
+            className='input w-full' 
+            name='email' />
+            <input type="password" 
+            placeholder=' ********' 
+            className='inpu w-fullt'
+             name='password' />
+            <button type='submit' className='btn space-y-4 w-full btn-1'>Login</button>
+          </form>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
